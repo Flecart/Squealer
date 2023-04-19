8a98c2801cc67dbfeb3f68d9bbb7460b4ef992aa
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-19 23:12:46 +0200

    feat(setup): setup husky commitlint and committizen

---
+++ commitlint.config.cjs
@@ -0,0 +1,10 @@
+// https://dev.to/edsonjuniornarvaes/commitizen-commitlint-3elg
+module.exports = { 
+    extends: ["@commitlint/config-conventional"] ,
+
+
+    /*
+     * Custom URL to show upon failure
+     */
+    helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+};
