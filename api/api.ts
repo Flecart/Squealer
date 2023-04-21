3698278f82d7da1558dd0168a2a9a34ee3e13c6a
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-21 20:58:11 +0200

    feat(api): move to tsoa apis

--- api/api.ts
+++
@@ -1,7 +0,0 @@
-import random from './prova/random.js';
-import hello from './hello.js';
-
-export default [
-    random,
-    hello
-]
--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,34 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import endpoint from '../config/endpoints.json' assert { type: "json" };
+import endpoint from '../config/endpoints.json';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import controllers from '../api/api.js';
+import { RegisterRoutes } from "../build/api/routes";
 
-import { attachControllers } from '@decorators/express';
 import initMongo from './mongo.js';
 
 // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-await initMongo();
-
-const server = express()
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
-
-const apiRouter = express.Router();
-attachControllers(apiRouter, controllers);
-server.use('/api', apiRouter);
-
-server.use(`/${endpoint.SMM}`, express.static(endpoint.SMM));
-
-server.use(`/${endpoint.DASHBOARD}`, express.static(path.resolve(__dirname, '../', endpoint.DASHBOARD))); 
-server.all(`/${endpoint.DASHBOARD}`, (_, res) => {
-    res.sendFile(path.resolve(__dirname, '../', endpoint.DASHBOARD, 'index.html'));
-});
-
-server.use(express.static(path.resolve(__dirname, '../app'))); 
-server.all('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, '../app', 'index.html'));
-});
-
-server.listen(3000, () => {
-    console.log('> Ready on http://localhost:3000')
+initMongo()
+.then(() => {
+    const server = express()
+    server.use(bodyParser.json())
+    server.use(bodyParser.urlencoded({ extended: true }))
+    
+    RegisterRoutes(server);
+    
+    server.use(`/${endpoint.SMM}`, express.static(endpoint.SMM));
+    
+    server.use(`/${endpoint.DASHBOARD}`, express.static(path.resolve(__dirname, '../', endpoint.DASHBOARD))); 
+    server.all(`/${endpoint.DASHBOARD}`, (_, res) => {
+        res.sendFile(path.resolve(__dirname, '../', endpoint.DASHBOARD, 'index.html'));
+    });
+    
+    server.use(express.static(path.resolve(__dirname, '../app'))); 
+    server.all('*', (_, res) => {
+        res.sendFile(path.resolve(__dirname, '../app', 'index.html'));
+    });
+    
+    server.listen(3000, () => {
+        console.log('> Ready on http://localhost:3000')
+    })
 })
