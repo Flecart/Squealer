9bffb2a8e2bf82251ef51b7fc0e5baf06fd67bc1
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-21 21:14:31 +0200

    feat(api): add swagger docs endpoint

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Response as ExResponse, Request as ExRequest } from 'express';
 import bodyParser from 'body-parser';
 import endpoint from '../config/endpoints.json';
 import path from 'path';
 import { RegisterRoutes } from "../build/api/routes";
 
+import swaggerUi from "swagger-ui-express";
 import initMongo from './mongo';
 
 // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules
@@ -15,18 +16,24 @@ initMongo()
     server.use(bodyParser.urlencoded({ extended: true }))
     
     RegisterRoutes(server);
+    server.use(`/${endpoint.APIDOCS}`, swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+        return res.send(
+            swaggerUi.generateHTML(await import("../build/swagger.json"))
+        );
+    });
     
     server.use(`/${endpoint.SMM}`, express.static(endpoint.SMM));
     
     server.use(`/${endpoint.DASHBOARD}`, express.static(path.resolve(__dirname, '../', endpoint.DASHBOARD))); 
-    server.all(`/${endpoint.DASHBOARD}`, (_, res) => {
+    server.all(`/${endpoint.DASHBOARD}`, (_req: ExRequest, res: ExResponse) => {
         res.sendFile(path.resolve(__dirname, '../', endpoint.DASHBOARD, 'index.html'));
     });
     
     server.use(express.static(path.resolve(__dirname, '../app'))); 
-    server.all('*', (_, res) => {
+    server.all('*', (_req: ExRequest, res: ExResponse) => {
         res.sendFile(path.resolve(__dirname, '../app', 'index.html'));
     });
+
     
     server.listen(3000, () => {
         console.log('> Ready on http://localhost:3000')
