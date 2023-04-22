7681b1527e1f0ed6fff0ed38a959cbc2305c7ff2
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-22 23:28:17 +0200

    feat(style): add config with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["standard-with-typescript", "eslint:recommended"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    curly: ["error", "all"],
-    "spaced-comment": ["error", "always", { block: { balanced: true } }],
-    "array-bracket-spacing": ["error", "never"],
-    "brace-style": ["error", "1tbs"],
-    "comma-dangle": ["error", "only-multiline"],
-    camelcase: ["error", { properties: "always" }],
-    semi: ["error", "always"],
-    quotes: ["error", "double"],
-    indent: ["error", 4],
-    "quote-props": ["error", "consistent"],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['standard-with-typescript', 'eslint:recommended', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
 };
