e7a9cd1e1d06ba0157a0714c51481c161ebf19b1
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-24 19:23:43 +0200

    feat(app): set eslint for react ts

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(): JSX.Element {
+    return (
+        <div>
+            <h1>My App</h1>
+            <p>My App is working!</p>
+        </div>
+    );
+}
+
+export default App;
