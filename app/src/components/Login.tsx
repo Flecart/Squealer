fdac9eab7803a5a8116687c7a5e4778e1203ce2f
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-25 18:35:31 +0200

    fix(app): login component now use states

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as userSelectors from '@flux/selectors/user';
 import * as userActions from '@flux/actions/user';
@@ -7,17 +7,15 @@ import { Button, Container, Form, FormGroup } from 'react-bootstrap';
 
 export default function Login(): JSX.Element {
     const dispatch = useDispatch();
+    const [formName, setFormName] = useState('');
+    const [formPassword, setFormPassword] = useState('');
 
     const handleCreateUser = useCallback(
         (event: React.FormEvent<HTMLFormElement>) => {
             event.preventDefault();
-            const name = event.currentTarget['username'].value;
-            const password = event.currentTarget['password'].value;
-            event.currentTarget['username'].value = '';
-            event.currentTarget['password'].value = '';
-            dispatch(userActions.createUser({ name, password }));
+            dispatch(userActions.createUser({ name: formName, password: formPassword }));
         },
-        [dispatch],
+        [dispatch, formName, formPassword],
     );
 
     const name = useSelector(userSelectors.getUserName);
@@ -26,11 +24,25 @@ export default function Login(): JSX.Element {
             <Form className="m-0 me-4 py-3 px-3 border" onSubmit={handleCreateUser}>
                 <FormGroup className="mb-3">
                     <Form.Label className="text-light">Username</Form.Label>
-                    <Form.Control name="username" type="text" placeholder="Inserisci il tuo username" />
+                    <Form.Control
+                        type="text"
+                        value={formName}
+                        onChange={(e) => {
+                            setFormName(e.target.value);
+                        }}
+                        placeholder="Inserisci il tuo username"
+                    />
                 </FormGroup>
                 <FormGroup className="mb-3">
                     <Form.Label className="text-light">Password</Form.Label>
-                    <Form.Control name="password" type="password" placeholder="Inserisci la tua password" />
+                    <Form.Control
+                        type="password"
+                        value={formPassword}
+                        onChange={(e) => {
+                            setFormPassword(e.target.value);
+                        }}
+                        placeholder="Inserisci la tua password"
+                    />
                 </FormGroup>
                 <Container className="d-flex justify-content-center">
                     <Button className="col-6 me-1" variant="outline-success" type="submit">
