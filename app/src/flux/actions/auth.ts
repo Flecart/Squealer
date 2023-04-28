e0957cfdcdea774322be1f76277564c76af394c8
Giovanni Spadaccini <giospadaccini74@gmail.com>
2023-04-28 20:27:33 +0200

    feat(app): added the display user redux

--- app/src/flux/actions/auth.ts
+++ app/src/flux/actions/auth.ts
@@ -1,5 +1,5 @@
 import { createAction } from 'typesafe-actions';
 
-export const createUserAuth = createAction('user/create_user')<{ display_name: string; password: string }>();
-export const loginUser = createAction('user/login')<{ username: string; password: string }>();
-export const authParameter = createAction('user/set_auth_Parameter')<{ username: string; jwt: string }>();
+export const createUserAuth = createAction('auth/create_user')<{ display_name: string; password: string }>();
+export const loginUser = createAction('auth/login')<{ username: string; password: string }>();
+export const authParameter = createAction('auth/set_auth_Parameter')<{ username: string; jwt: string }>();
