2117cd5cf737234cc8ea29968dd64d6778ec9497
Giovanni Spadaccini <giospadaccini74@gmail.com>
2023-05-01 11:36:30 +0200

    fix(app): deleted old flux files

--- app/src/flux/actions/user.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'typesafe-actions';
-
-export const createUser = createAction('user/create_user')<{ display_name: string; password: string }>();
-export const loginUser = createAction('user/login')<{ username: string; password: string }>();
-export const authParameter = createAction('user/set_auth_Parameter')<{ username: string; jwt: string }>();
--- app/src/flux/selectors/user.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from 'reselect';
-import { type IStore } from '@flux/store';
-import { type UserStore } from '@flux/StoreInterfaces';
-
-export const selectUserStore = (state: IStore): UserStore => state.user;
-export const getUserName = createSelector(selectUserStore, (user) => user.username);
