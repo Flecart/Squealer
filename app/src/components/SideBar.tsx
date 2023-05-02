6d111d22bb104426f64153487676a1c08a37a34a
gio <giospadaccini74@gmail.com>
2023-05-02 10:25:20 +0000

    fix(app): removed theme switcher and fragments

--- app/src/components/SideBar.tsx
+++ app/src/components/SideBar.tsx
@@ -2,12 +2,11 @@ import { Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 
-import { AuthContext, ThemeContext } from '../contexts';
+import { AuthContext } from '../contexts';
 
 export function SideBar(): JSX.Element {
     const [authState] = useContext(AuthContext);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-    const [_, toggleTheme] = useContext(ThemeContext);
 
     return (
         <Navbar className="d-none d-lg-flex flex-column align-items-start ps-3" sticky="top">
@@ -31,14 +30,6 @@ export function SideBar(): JSX.Element {
                     <Button className="rounded">Login</Button>
                 </Link>
             )}
-            <Button
-                className="rounded"
-                onClick={() => {
-                    toggleTheme();
-                }}
-            >
-                Toggle theme
-            </Button>
         </Navbar>
     );
 }
