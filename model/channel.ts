cd6d38acd0adba495654ee492a9f3415c9f2a0c6
gio <giospadaccini74@gmail.com>
2023-05-02 11:37:01 +0000

    fix(db): changed channel managing

--- model/channel.ts
+++ model/channel.ts
@@ -18,56 +18,29 @@ export function isMultiOwnerChannel(channel: IChannel): boolean {
     return !isSingleOwnerChannel(channel);
 }
 
-export interface IUserChannel {
-    type: 'user' | 'public';
-    ownerRef: mongoose.Types.ObjectId;
-}
-
-export const UserChannelScheme = new mongoose.Schema<IUserChannel>({
-    type: { type: String, required: true },
-    ownerRef: { type: mongoose.Schema.Types.ObjectId, required: true, ref: UserModelName },
-});
-
-export interface IOwnedChannel {
-    type: 'owned' | 'squealer';
-    ownerRef: mongoose.Types.ObjectId[];
-    users: [
-        {
-            userRef: mongoose.Types.ObjectId;
-            permission: permissionType;
-        },
-    ];
-    admins: mongoose.Types.ObjectId[];
-}
-
-export const OwnedChannelScheme = new mongoose.Schema<IOwnedChannel>({
-    type: { type: String, required: true },
-    ownerRef: { type: [mongoose.Schema.Types.ObjectId], required: true, ref: UserModelName },
-    users: {
-        type: [
-            {
-                userRef: { type: mongoose.Schema.Types.ObjectId, required: true, ref: UserModelName },
-                permission: { type: Number, required: true },
-            },
-        ],
-        required: true,
-    },
-    admins: { type: [mongoose.Schema.Types.ObjectId], required: true, ref: UserModelName },
-});
-
 // user e public sono userchannels, altri sono ownedchannels
 export interface IChannel {
     name: string;
     description: string;
-    members: IUserChannel | IOwnedChannel; // tutti i dati relativi alle persone nel canale
+    type: channelType;
+    owner: mongoose.Schema.Types.ObjectId;
+    users: {
+        user: mongoose.Schema.Types.ObjectId[];
+        privilage: number;
+    };
     messages: IMessage; // TODO: create tipo per i messaggi, che mettiamo qui
 }
 
 const ChannelSchema = new mongoose.Schema<IChannel>({
     name: { type: String, required: true },
     description: { type: String, required: false },
-    members: { type: Object, required: true }, // TODO: add validations for the object
     messages: { type: [String], required: true },
+    type: { type: String, required: true },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: UserModelName, required: true },
+    users: {
+        user: [{ type: mongoose.Schema.Types.ObjectId, ref: UserModelName, required: true }],
+        privilage: { type: Number, required: true },
+    },
 });
 
 export default mongoose.model<IChannel>('Channel', ChannelSchema);
