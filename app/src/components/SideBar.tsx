d88d06397676fbe150e9c532673fdb57f2cf8561
gio <giospadaccini74@gmail.com>
2023-05-02 22:19:08 +0200

    feat(app): added logout e layout (#36)
    * feat(app): added logout e layout
    
    * fix(app): fixed pr requirements
    
    * fix(app): fixed pr requirements
    
    * fix(app): removed theme switcher and fragments

--- app/src/components/SideBar.tsx
+++ app/src/components/SideBar.tsx
@@ -2,12 +2,11 @@ import { Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 
-import { AuthContext, ThemeContext } from '../contexts';
+import { AuthContext } from '../contexts';
 
 export function SideBar(): JSX.Element {
     const [authState] = useContext(AuthContext);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-    const [_, toggleTheme] = useContext(ThemeContext);
 
     return (
         <Navbar className="d-none d-lg-flex flex-column align-items-start ps-3" sticky="top">
@@ -17,19 +16,20 @@ export function SideBar(): JSX.Element {
             <Link to="#">
                 <Button className="rounded">Impostazioni</Button>
             </Link>
-            {authState == null && (
+            {authState !== null ? (
+                <>
+                    <Link to="/logout">
+                        <Button className="rounded">Logout</Button>
+                    </Link>
+                    <Link to={`/user/${authState.username}`}>
+                        <Button className="rounded">Me</Button>
+                    </Link>
+                </>
+            ) : (
                 <Link to="/login">
                     <Button className="rounded">Login</Button>
                 </Link>
             )}
-            <Button
-                className="rounded"
-                onClick={() => {
-                    toggleTheme();
-                }}
-            >
-                Toggle theme
-            </Button>
         </Navbar>
     );
 }
---
+++ app/src/views/Logout.tsx
@@ -0,0 +1,37 @@
+import { Alert, Button, Container } from 'react-bootstrap';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts';
+import { useNavigate } from 'react-router-dom';
+import SidebarSearchLayout from 'src/layout/SidebarSearchLayout';
+export default function Logout(): JSX.Element {
+    const [authState, setAuthState] = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    function handleLogout(): void {
+        setAuthState(null);
+    }
+
+    return (
+        <SidebarSearchLayout>
+            <Container className="d-flex justify-content-center">
+                {authState === null ? (
+                    <Container>
+                        <Alert variant="danger">Non sei loggato</Alert>
+                        <Button
+                            onClick={() => {
+                                navigate('/login');
+                            }}
+                        >
+                            LogOut
+                        </Button>
+                    </Container>
+                ) : (
+                    <Container className="d-flex justify-content-center">
+                        <Alert variant="light">Attualmente sei loggato come {authState?.username}</Alert>
+                        <Button onClick={handleLogout}>LogOut</Button>
+                    </Container>
+                )}
+            </Container>
+        </SidebarSearchLayout>
+    );
+}
