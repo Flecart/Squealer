aeb750c8aba5f78c1cc3794e6bb6f075e649875d
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-05-03 11:23:14 +0200

    feat(api): creato endpoints per cambiare password e username
    No
    
    #37 e anche un altro

--- api/login/loginController.ts
+++ api/login/loginController.ts
@@ -1,7 +1,8 @@
-import { Body, Post, Route, Response, SuccessResponse, Controller } from '@tsoa/runtime';
+import { Body, Post, Route, Request, Response, SuccessResponse, Controller, Security } from '@tsoa/runtime';
 import { LoginService } from './loginService';
 import { type AuthResponse } from '@model/auth';
 import { HttpError } from '@model/error';
+import { getUserFromRequest } from '@api/utils';
 
 export interface Credentials {
     username: string;
@@ -22,5 +23,31 @@ export class LoginController extends Controller {
     @SuccessResponse<AuthResponse>(201, 'Created')
     public async createUser(@Body() credentials: Credentials): Promise<AuthResponse> {
         return new LoginService().createUser(credentials.username, credentials.password);
+    }
+
+    @Post('user/{username}/change-password')
+    @Security('jwt')
+    @Response<HttpError>(400, 'Bad request')
+    @SuccessResponse<AuthResponse>(200, 'Password changed')
+    public async changePassword(
+        @Body() password_change: { old_password: string; new_password: string },
+        @Request() request: any,
+    ): Promise<{ message: string }> {
+        return new LoginService().changePassword(
+            password_change.old_password,
+            password_change.new_password,
+            getUserFromRequest(request),
+        );
+    }
+
+    @Post('user/{username}/change-username')
+    @Security('jwt')
+    @Response<HttpError>(400, 'Bad request')
+    @SuccessResponse<AuthResponse>(200, 'Username changed')
+    public async changeUsername(
+        @Body() new_username: { new_username: string },
+        @Request() request: any,
+    ): Promise<{ message: string }> {
+        return new LoginService().changeUsername(new_username.new_username, getUserFromRequest(request));
     }
 }
