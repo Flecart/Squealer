783cf2f3062dafa5d112315789c82f9f3ee35c28
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-05-07 12:26:12 +0200

    chore(api): remove storage config files

--- config/api.ts
+++ config/api.ts
@@ -5,7 +5,3 @@ export const DEFAULT_QUOTA: IQuotas = {
     month: 1000,
     week: 20000,
 };
-
-const rootDir = require('path').resolve('./');
-export const CLIENT_UPLOAD_DIR = `uploads/`; // utilizzato per accedere al file dal client
-export const DEFAULT_UPLOAD_DIR = `${rootDir}/build/app/${CLIENT_UPLOAD_DIR}`;
