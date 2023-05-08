3a36733c88e1bb53c49920229fb2d3619de5435f
Luco <118299422+Lukirby@users.noreply.github.com>
2023-05-08 10:44:07 +0200

    Feat/settings (#54)
    * refactor(app): added setting [incompleted]
    
    * refactor(app): added setting [incompleted]
    
    * refactor(app): rebased branch to main
    
    * feat(app): created setting
    
    * feat(app): added Offcanvas that shows Manu-Bar
    
    ---------
    
    Co-authored-by: Lukirby <pana.luca@gmail.com>

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,31 +1,55 @@
-import { Nav, Navbar, Container, Form } from 'react-bootstrap';
+import { useState } from 'react';
+import { SideBar } from './SideBar';
+import { Navbar, Container, Offcanvas } from 'react-bootstrap';
+// import { Nav, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 export function Header(): JSX.Element {
     const navigator = useNavigate();
+    const [show, setShow] = useState(false);
+    const handleClose = (): void => {
+        setShow(false);
+    };
+    const handleShow = (): void => {
+        setShow(true);
+    };
+
     return (
         <Navbar className="container-fluid" expand="lg" sticky="top">
-            <Container className="d-flex flex-row justify-content-center">
+            <Container className="d-flex flex-row justify-content-evenly px-3">
+                <Navbar.Toggle onClick={handleShow} className="d-md-none bg-primary" />
+
+                <Offcanvas show={show} onHide={handleClose}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Menu</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <SideBar />
+                    </Offcanvas.Body>
+                </Offcanvas>
+
                 <Navbar.Brand
+                    className="text-white flex-grow-1 ps-3 text-center"
                     onClick={() => {
                         navigator('/');
                     }}
                 >
                     Squealer
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav border" />
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav border" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="">
                         <Nav.Link href="#home"> Esplora </Nav.Link>
-                        {/* <NavDropdown title="Canali">
+                        <NavDropdown title="Canali">
                             <NavDropdown.Item>Canale 1</NavDropdown.Item>
                             <NavDropdown.Item>Canale 2</NavDropdown.Item>
-                        </NavDropdown> */}
+                        </NavDropdown>
                         <Form className="">
                             <Form.Control type="search" placeholder="Cerca un Utente" />
                         </Form>
                     </Nav>
-                </Navbar.Collapse>
+                </Navbar.Collapse> 
+                */}
             </Container>
         </Navbar>
     );
---
+++ app/src/views/Settings.tsx
@@ -0,0 +1,27 @@
+import { Container, Stack } from 'react-bootstrap';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts';
+import { Link } from 'react-router-dom';
+import SidebarSearchLayout from 'src/layout/SidebarSearchLayout';
+
+export default function Settings(): JSX.Element {
+    const [authState] = useContext(AuthContext);
+
+    return (
+        <SidebarSearchLayout>
+            <Container className="d-flex justify-content-center">
+                {authState === null ? (
+                    <Stack className="d-flex rounded border p-2">
+                        <Link to="/user/changepass">Cambio Password</Link>
+                        <Link to="/user/reset">Reset Password</Link>
+                        <Link to="/user/delete" className="text-danger">
+                            Clicca qui per eliminare account
+                        </Link>
+                    </Stack>
+                ) : (
+                    <Container>Cose Da Aggiungere</Container>
+                )}
+            </Container>
+        </SidebarSearchLayout>
+    );
+}
