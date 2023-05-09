4ecf6f1c6b31af63ef0c79cec290492727641760
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-05-09 18:05:05 +0200

    feat(app): make map work

---
+++ app/src/components/Map.tsx
@@ -0,0 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// fixes: https://stackoverflow.com/questions/49441600/react-leaflet-marker-files-not-found
+const DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+export interface MapProps {
+    lat: number;
+    lng: number;
+}
+
+function Map({ lat, lng }: MapProps): JSX.Element {
+    return (
+        <MapContainer style={{ height: '15rem' }} center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[lat, lng]}>
+                <Popup>gio is here</Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+
+export default Map;
