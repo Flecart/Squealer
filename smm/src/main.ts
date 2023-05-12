e541b66e01101cffed548069a9196d214ca9ab15
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-05-12 21:40:45 +0200

    feat(smm): add boostrap compatibility

--- smm/src/main.ts
+++ smm/src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-import './assets/main.css'
+import './assets/app.scss'
+
+// // Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
 
 createApp(App).mount('#app')
--- smm/vite.config.ts
+++ smm/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -11,7 +10,8 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      vue: '@vue/compat'
     }
   },
   base: `/${endpoints.SMM}`,
