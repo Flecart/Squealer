df074781c7d508282418f123f9ae19a8f8894dee
Angelo Huang <63465494+Flecart@users.noreply.github.com>
2023-06-06 21:16:48 +0200

    feat(logger): create logger (#95)
    Co-authored-by: gio <giospadaccini74@gmail.com>

--- api/upload/uploadController.ts
+++ api/upload/uploadController.ts
@@ -2,13 +2,17 @@ import { Post, SuccessResponse, Response, UploadedFile, Route, Controller } from
 import { HttpError } from '@model/error';
 import { UploadService } from './uploadService';
 
+import logger from '@server/logger';
+
+const uploadLogger = logger.child({ label: 'upload' });
+
 @Route('/upload')
 export class UploadController extends Controller {
     @Post('')
     @Response<HttpError>(400, 'Bad Request')
     @SuccessResponse(200, 'Upload file')
     public async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<any> {
-        console.info(`UploadController.uploadFile with name '${file.originalname}'`);
+        uploadLogger.info(`[uploadFile] with name '${file.originalname}'`);
 
         return await new UploadService().uploadFile(file);
     }
