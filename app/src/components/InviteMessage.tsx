fef201162403d861dd31e43c1ba2f5379f19e784
Giovanni Spadaccini - giovanni.spadaccini3@studio.unibo.it <giovanni.spadaccini3@labs.cs.unibo.it>
2023-06-06 11:23:39 +0200

    feat(app): added leave and join button, add a member and accept invite

--- app/src/components/InviteMessage.tsx
+++ app/src/components/InviteMessage.tsx
@@ -2,6 +2,11 @@
 import { type IMessage, type Invitation } from '@model/message';
 import { Button, Card, Container, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../contexts';
+import { fetchApi } from '../api/fetch';
+import { apiChannelBase } from 'src/api/routes';
+import { type ChannelResponse } from '@model/channel';
 
 interface PostProps {
     message: IMessage;
@@ -12,24 +17,65 @@ function InviteMessage({ message }: PostProps): JSX.Element {
         console.log('should not be displayed');
         return <></>;
     }
-    const userUrl = 'user/' + message.creator;
-    const invitation = message.content.data as Invitation;
-    const channelUrl = 'channel/' + invitation.channel;
+    const [hide, setHide] = useState(false);
+    const [auth] = useContext(AuthContext);
+    const userUrl = `user/${message.creator}`;
+    const invitation: Invitation = message.content.data as Invitation;
+    const channelUrl = `channel/${invitation.channel}`;
+    const accept = (): void => {
+        fetchApi<ChannelResponse>(
+            `${apiChannelBase}/accept`,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    messageID: message._id.toString(),
+                }),
+            },
+            auth,
+            () => {
+                setHide(true);
+                // maybe we can navigate to that channel
+            },
+            (_) => {},
+        );
+    };
+    const decline = (): void => {
+        fetchApi<ChannelResponse>(
+            `${apiChannelBase}/decline`,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    messageID: message._id.toString(),
+                }),
+            },
+            auth,
+            () => {
+                setHide(true);
+                // maybe we can navigate to that channel
+            },
+            (_) => {},
+        );
+    };
 
     return (
-        <Card body>
-            <Stack className="" as="article" role="article" gap={3}>
-                <Container className="d-flex justify-content-center ">
-                    <span>
-                        {"L'utente "} <Link to={userUrl}>{message.creator}</Link> ti ha invitato in ad unirti al canale
-                        <Link to={channelUrl}> {invitation.channel} </Link>
-                    </span>
-                </Container>
-                <Stack direction="horizontal" gap={4} className="justify-content-center">
-                    <Button>Accetto</Button> <Button>Declino</Button>{' '}
-                </Stack>
-            </Stack>
-        </Card>
+        <>
+            {!hide && (
+                <Card body>
+                    <Stack className="" as="article" role="article" gap={3}>
+                        <Container className="d-flex justify-content-center ">
+                            <span>
+                                {"L'utente "} <Link to={userUrl}>{message.creator}</Link> ti ha invitato in ad unirti al
+                                canale
+                                <Link to={channelUrl}> {invitation.channel} </Link>
+                            </span>
+                        </Container>
+                        <Stack direction="horizontal" gap={4} className="justify-content-center">
+                            <Button onClick={accept}>Accetto</Button> <Button onClick={decline}>Declino</Button>{' '}
+                        </Stack>
+                    </Stack>
+                </Card>
+            )}
+        </>
     );
 }
 
