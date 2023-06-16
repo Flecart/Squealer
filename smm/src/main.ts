56a6bb84b1fef20dcbbf17bf579edd2cee2945f7
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-06-16 21:19:14 +0200

    feat(smm): now full compatibility!

--- smm/src/main.ts
+++ smm/src/main.ts
@@ -1,61 +1,34 @@
 import Vue, { createApp } from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import * as VueRouter from 'vue-router'
-import Dashboard from './components/Dashboard.vue'
-import { getClientsRoute, squealerBaseURL } from './routes'
-// @ts-ignore outside of root directory
-import endpoints from '../../config/endpoints.json'
+import { getClientsRoute, router, redirectToLogin } from './routes'
 import { authInject, clientInject } from './keys'
 
 import './assets/app.scss'
 import type { IUser } from '@model/user'
-import BuyQuotaVue from './views/BuyQuotaView.vue'
 
-const routes = [
-  { path: `/${endpoints.SMM}`, name: 'main', component: Dashboard },
-  { path: `/${endpoints.SMM}/buy-quota`, name: 'about', component: BuyQuotaVue }
-]
-
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory(),
-  routes
-})
-
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : squealerBaseURL
+// gently provided by https://stackoverflow.com/questions/51292406/check-if-token-expired-using-this-jwt-library
+const isTokenExpired = (token: string) =>
+  Date.now() >= JSON.parse(window.atob(token.split('.')[1])).exp * 1000
 
 const app = createApp(App)
 
 const authState = JSON.parse(localStorage.getItem('auth') ?? 'null')
 if (authState != null) {
+  if (isTokenExpired(authState.token)) {
+    redirectToLogin()
+  }
+
   app.provide(authInject, authState)
-  // IIFE so that i can use await in top level
   const response = await fetch(getClientsRoute, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: 'Bearer ' + authState.token
     }
   })
-  const jsonResponse = await response.json()
+  const jsonResponse = await (response as Response).json()
   app.provide(clientInject, jsonResponse as IUser[])
 }
-
-router.beforeEach((to, _) => {
-  const targetPath = to.path
-  const authState = JSON.parse(localStorage.getItem('auth') ?? 'null')
-
-  if (authState == null) {
-    // in dev mode set the auth by hand, localstorage won't work with different ports
-    window.location.replace(
-      `${squealerBaseURL}/login?redirect=${encodeURIComponent(baseUrl + targetPath)}`
-    )
-  }
-
-  if (targetPath == '/logout') {
-    // redirect to main after logout, ci sono cose brutte col localstorage :(
-    window.location.replace(`${squealerBaseURL}/logout?redirect=${encodeURIComponent(baseUrl)}`)
-  }
-})
 
 // @ts-ignore
 Vue.use(BootstrapVue).use(IconsPlugin)
--- smm/src/routes.ts
+++ smm/src/routes.ts
@@ -1,3 +1,10 @@
+import * as VueRouter from 'vue-router'
+import Dashboard from './components/Dashboard.vue'
+import BuyQuotaVue from './views/BuyQuotaView.vue'
+
+// @ts-ignore outside of root directory
+import endpoints from '../../config/endpoints.json'
+
 // TODO: mettere l'indirizzo del server di squealer se non dev, quando si saprà l'indirizzo di squealer
 export const squealerBaseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
 
@@ -5,3 +12,34 @@ export const getClientsRoute = `${squealerBaseURL}/api/smm/clients`
 export const buyQuotaBaseRoute = `${squealerBaseURL}/api/smm/buy-quota`
 export const getClientMessageBaseRoute = `${squealerBaseURL}/api/message/user`
 export const getUserBaseRoute = `${squealerBaseURL}/api/user`
+
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : squealerBaseURL
+export const redirectToLogin = () => {
+  // in dev mode set the auth by hand, localstorage won't work with different ports
+  window.location.replace(`${squealerBaseURL}/logout?redirect=${encodeURIComponent(baseUrl)}`)
+}
+
+const routes = [
+  { path: `/${endpoints.SMM}`, name: 'main', component: Dashboard },
+  { path: `/${endpoints.SMM}/buy-quota`, name: 'about', component: BuyQuotaVue }
+]
+
+export const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, _) => {
+  const targetPath = to.path
+  const authState = JSON.parse(localStorage.getItem('auth') ?? 'null')
+
+  if (authState == null) {
+    redirectToLogin()
+  }
+
+  if (targetPath == '/logout') {
+    // redirect to main after logout, ci sono cose brutte col localstorage :(
+    localStorage.removeItem('auth')
+    redirectToLogin()
+  }
+})
