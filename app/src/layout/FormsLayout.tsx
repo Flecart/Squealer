ec16ac17768e548d4fe27f95b17f67c8f03af6ad
Lukirby <pana.luca@gmail.com>
2023-06-21 13:58:56 +0200

    style(app): styled main form plus new danger color with more contrast

---
+++ app/src/layout/FormsLayout.tsx
@@ -0,0 +1,21 @@
+export const formFormBS = 'd-flex flex-column align-items-stretch container-fluid m-0 p-3 border rounded-3';
+
+export const inputFormBS = 'mb-3';
+
+export const buttonFormBS = 'col-6 align-self-center mt-1';
+
+export const containerFormBS = 'd-flex flex-column align-items-center';
+
+export const titleFormBS = 'm-0 text-center';
+
+export const imageFormBS = 'd-inline-block';
+
+export const alertFormBS = 'm-3 mb-1';
+
+export const alertSuccessFormBS = '';
+
+export const spinnerFormBS = 'm-1 align-self-center';
+
+export const subTitleFormBS = 'text-center ';
+
+export const linksFormBS = 'link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover';
