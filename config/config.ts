4404946210b949ac6fa69543a2a9f94eacae7019
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-06-29 14:57:21 +0200

    fix(deploy): hard code computer mongo

--- config/config.ts
+++ config/config.ts
@@ -8,9 +8,9 @@ export const JWT_SECRET = new TextEncoder().encode(process.env['JWT_TOKEN'] || '
 export const PASS_HASH = new TextEncoder().encode(process.env['PASS_HASH'] || 'secret');
 export const JWT_ALG = process.env['JWT_ALG'] || 'HS256';
 export const DEV_DIR = process.env['NODE_ENV'] === 'development' ? 'build/' : '';
-export const MONGO_USER = process.env['MONGO_USER']; // TODO aggiungerci quelle di defualt della build del server
-export const MONGO_PASS = process.env['MONGO_PASS'];
-export const MONGO_DOMAIN = process.env['MONGO_DOMAIN'];
+export const MONGO_USER = process.env['MONGO_USER'] || 'site222307';
+export const MONGO_PASS = process.env['MONGO_PASS'] || 'oMahvo1a';
+export const MONGO_DOMAIN = process.env['MONGO_DOMAIN'] || 'mongo_site222307';
 export const JWT_ISSUER = process.env['JWT_ISSUER'] || 'urn:squealer:issuer';
 export const PORT = process.env['PORT'] || 8000;
 export const ENABLE_CROSS_ORIGIN = process.env['CROSS_ORIGIN'] === 'true' || false;
