e5d243a6bb1fb6b2a4c573761dbdfe5d35cf3b82
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-06-29 14:52:18 +0200

    feat(smm): create login buttons

--- smm/src/views/PostMessageView.vue
+++ smm/src/views/PostMessageView.vue
@@ -8,6 +8,7 @@ import type { IQuotas } from '@model/quota'
 const { currentClient, setClient } = inject(currentClientInject)!
 
 const usedQuota = ref<number>(0)
+const fileInputRef = ref<HTMLInputElement>()
 
 const remainingQuota = computed<IQuotas>(() => {
   return {
@@ -28,6 +29,12 @@ watch(textInput, (newValue, _oldValue) => {
 const handleSubmit = () => {
   console.log('submit')
 }
+
+const selectFileInput = () => {
+  // console.log('selectFileInput')
+  console.log(fileInputRef.value)
+  fileInputRef.value?.click()
+}
 </script>
 
 <template>
@@ -37,21 +44,56 @@ const handleSubmit = () => {
 
   <b-form class="form" @submit.prevent="handleSubmit">
     <label class="sr-only" for="inline-form-input-channel">Channel</label>
-    <b-input-group prepend="Channel" class="mb-2 mr-sm-2 mb-sm-0">
+    <b-input-group prepend="Channel" class="mb-2 mt-2 mr-sm-2 mb-sm-0">
       <b-form-input
         v-model="channelInput"
         id="inline-form-input-channel"
-        placeholder="the channel you want to post to"
+        placeholder="Enter Channel name"
       ></b-form-input>
     </b-input-group>
 
-    <b-form-textarea
-      v-model="textInput"
-      placeholder="Enter something to send..."
-      rows="3"
-      max-rows="6"
-    ></b-form-textarea>
+    <div class="mt-3">
+      <b-form-textarea
+        v-model="textInput"
+        placeholder="Write your message here. You can also send image or video with the button below."
+        rows="3"
+        max-rows="6"
+      ></b-form-textarea>
+    </div>
+
+    <div class="d-flex justify-content-between align-items-center mx-3 mt-3">
+      <b-button type="submit" variant="primary">Send</b-button>
+
+      <b-button
+        pill
+        class="rounded-3"
+        variant="dark"
+        aria-label="click to send image"
+        title="send image"
+        @click="selectFileInput"
+      >
+        <b-form-file
+          ref="fileInputRef"
+          accept="image/*, video/*"
+          aria-hidden="true"
+          class="d-none"
+        ></b-form-file>
+        <b-icon-file-earmark-image-fill></b-icon-file-earmark-image-fill>
+      </b-button>
+    </div>
   </b-form>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import 'bootstrap/scss/bootstrap.scss';
+
+.form {
+  max-width: 100vw;
+}
+
+@media screen and (min-width: map-get($grid-breakpoints, md)) {
+  .form {
+    max-width: 50vw;
+  }
+}
+</style>
