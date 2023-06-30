4bc4a63f22f3756fe194a02126b24741a0a4611a
Angelo Huang <63465494+Flecart@users.noreply.github.com>
2023-06-30 16:09:23 +0200

    Deploy/university computers (#158)
    * feat(app): first unworking version of file upload
    
    * feat(api): upload
    
    * feat(mode): remove file model
    
    * Revert "feat(mode): remove file model"
    
    This reverts commit 9038d54f2277a9c18af26efd316c3d6a5a594d78.
    
    * chore(api): rename to file
    
    * chore(api): remove storage config files
    
    * feat(api): create file uploader
    
    * fix(deploy): hard code computer mongo
    
    * fix(deploy): try to create default upload dir
    
    * fix(server): remove storage
    
    * feat(server): add file uploads
    
    * fix: remove upload dir from package.json
    
    * fix(app): remove upload images
    
    * refactor(app): remove upload image component

---
+++ model/file.ts
@@ -0,0 +1,6 @@
+export interface IFile {
+    name: string;
+    size: number;
+    mimetype: string;
+    buffer: Buffer;
+}
--- server/mongo.ts
+++ server/mongo.ts
@@ -1,17 +1,23 @@
 import mongoose from 'mongoose';
 import logger from './logger';
+import { MONGO_USER, MONGO_DOMAIN, MONGO_PASS } from '@config/config';
 
 const mongoLogger = logger.child({ label: 'mongo' });
 
 export default function initConnection(): Promise<typeof mongoose> {
     mongoLogger.info('Connecting to mongo');
-    if (process.env['MONGO_USER'] === 'flecart') {
-        const mongoStringLocal = 'mongodb://root:example@localhost:27017/';
-        mongoLogger.info(`detected local mongo setting by env-var, connecting to ${mongoStringLocal}`);
-        return mongoose.connect(mongoStringLocal);
+    let connectionString = '';
+    mongoLogger.info(`envUser: ${MONGO_USER}`);
+    if (MONGO_USER === 'flecart') {
+        connectionString = 'mongodb://root:example@localhost:27017/';
+        mongoLogger.info(`detected local mongo setting by env-var, connecting to ${connectionString}`);
+    } else if (MONGO_USER === 'site222307') {
+        connectionString = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_DOMAIN}?authSource=admin&writeConcern=majority`;
+        mongoLogger.info(`detected university mongo setting by env-var, connecting to ${connectionString}`);
+    } else {
+        // connect to cloud instance
+        connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_DOMAIN}.mongodb.net/`;
     }
 
-    return mongoose.connect(
-        `mongodb+srv://${process.env['MONGO_USER']}:${process.env['MONGO_PASS']}@${process.env['MONGO_DOMAIN']}.mongodb.net/`,
-    );
+    return mongoose.connect(connectionString);
 }
